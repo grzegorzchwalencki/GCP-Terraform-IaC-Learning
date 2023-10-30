ad578fb9e87d264144006adacd97096cd59ec98a
greg-buka <bukacieszuka1@gmail.com>
2023-10-30 22:13:30 +0100

    Create explicit dependency

---
+++ Lab02-creating-resource-dependencies-with-terraform/exp.tf
@@ -0,0 +1,29 @@
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+
+  name         = "terraform-instance-2"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+}
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "qwiklabs-gcp-01-10fa29a325ea"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
