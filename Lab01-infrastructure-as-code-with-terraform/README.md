2e4e08435dd85c61f83f137f612fb2d5182c7c94
greg-buka <bukacieszuka1@gmail.com>
2023-10-30 20:55:49 +0100

    Update lab01

---
+++ Lab01-infrastructure-as-code-with-terraform/README.md
@@ -0,0 +1,6 @@
+# Lab Objectives
+
+In this lab, i learn how to perform the following tasks:
+
+* Define Google Cloud as the provider
+* Create, change, and destroy Google Cloud resources by using Terraform
---
+++ Lab01-infrastructure-as-code-with-terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+
+  project = "Project ID"
+  region  = "Region"
+  zone    = "Zone"
+}
+
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "e2-medium"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+ }
+ allow_stopping_for_update = true
+}
+
